1c271b9756515a98141a9fde5c31afa3c3bdcbb1
Rohan Nagar <46292211+Rohan17nagar@users.noreply.github.com>
2020-01-23 22:02:30 +0530

    Create SumOfNum.c

---
+++ SumOfNum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int sum,a,b,c,d,e;
+    printf("Enter the numbers:\n");
+    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+    sum = a+b+c+d+e;
+    printf("%d",sum);
+    return 0;
+}
